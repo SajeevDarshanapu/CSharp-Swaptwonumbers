ede34ed797d34686a67799a82a5819ba7e1de5f5
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-02-27 15:24:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Hey
+{
+    public class Hola
+    {
+        public static void Main(string[] args)
+        {
+            int a = 2, b = 4;
+            Console.WriteLine("Before Swapping a={0},b={1}",a, b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After Swapping a={0},b={1}", a, b);
+
+        }
+    }
+}
+
+
